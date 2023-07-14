4b1a5a8ea26e13ad37cdc53f5060b0e74c461674
l14nost <amirbanor@gmail.com>
2023-07-14 18:32:08 +0300

    create project

---
+++ src/main/java/com/spacelab/MyHouse24/MyHouse24Application.java
@@ -0,0 +1,13 @@
+package com.spacelab.MyHouse24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHouse24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyHouse24Application.class, args);
+	}
+
+}
