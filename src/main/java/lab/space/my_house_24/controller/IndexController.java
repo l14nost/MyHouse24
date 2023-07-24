773b977daade80d9bdbaeba88b3f2456ef9d0b70
l14nost <amirbanor@gmail.com>
2023-07-24 13:35:39 +0300

    fix after merge
    (cherry picked from commit 8adbabd699741ebdf051e59fea2c2bb927239668)

--- src/main/java/lab/space/my_house_24/controller/IndexController.java
+++ src/main/java/lab/space/my_house_24/controller/IndexController.java
@@ -1,4 +1,4 @@
-package com.spacelab.MyHouse24.controller;
+package lab.space.my_house_24.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
