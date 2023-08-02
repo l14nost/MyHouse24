a6283cdaa444db3cbcf01a030a6a90c732a79a5f
l14nost <amirbanor@gmail.com>
2023-08-02 15:38:43 +0300

    enums fix multilang

--- src/main/java/lab/space/my_house_24/enums/UserStatus.java
+++ src/main/java/lab/space/my_house_24/enums/UserStatus.java
@@ -1,18 +1,20 @@
 package lab.space.my_house_24.enums;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.i18n.LocaleContextHolder;
 
 import java.util.Locale;
 
 @RequiredArgsConstructor
 public enum UserStatus {
-    ACTIVE("Active", "Активный"),
-    NEW("New", "Новый"),
-    DISABLED("Disabled", "Отключен");
+    ACTIVE("Active", "Активний"),
+    NEW("New", "Новий"),
+    DISABLED("Disabled", "Відключений");
     private final String nameEn;
     private final String nameUk;
 
-    public String getUserStatus(Locale locale) {
+    public String getUserStatus() {
+        Locale locale = LocaleContextHolder.getLocale();
         if (locale.getLanguage().equalsIgnoreCase("uk")) {
             return nameUk;
         }
