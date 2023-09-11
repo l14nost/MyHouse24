73605b737abd57def127d882270c49f60642e174
l14nost <amirbanor@gmail.com>
2023-09-11 13:37:49 +0300

    some fix

--- src/main/java/lab/space/my_house_24/mapper/ApartmentMapper.java
+++ src/main/java/lab/space/my_house_24/mapper/ApartmentMapper.java
@@ -11,15 +11,10 @@ public class ApartmentMapper {
     }
 
     public static ApartmentResponse entityToDtoForMainPage(Apartment apartment) {
-//        BigDecimal balance = BigDecimal.ZERO;
-//        for (int i = 0; i< apartment.getBill().size(); i++){
-//            if (apartment.getBill().get(i).getDraft()) {
-//                balance = balance.add(apartment.getBill().get(i).getTotalPrice());
-//            }
-//            else {
-//                balance = balance.subtract(apartment.getBill().get(i).getTotalPrice());
-//            }
-//        }
+        BigDecimal balance = BigDecimal.ZERO;
+       if (apartment.getBankBook()!=null){
+           balance = apartment.getBankBook().getTotalPrice();
+       }
 
         return ApartmentResponse.builder()
                 .id(apartment.getId())
@@ -28,7 +23,7 @@ public class ApartmentMapper {
                 .owner(apartment.getUser().getLastname() + " " + apartment.getUser().getFirstname() + " " + apartment.getUser().getSurname())
                 .floor(apartment.getFloor().getName())
                 .section(apartment.getSection().getName())
-                .balance(BigDecimal.ZERO)
+                .balance(balance)
                 .build();
     }
 
